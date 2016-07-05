5dce6a6fc1234ce8c379c4b87bd8f9d1887b08ef
Ben Harper <benharper888@gmail.com>
2016-07-05 17:36:35 +1000

    added chunk generation

--- include/world.hpp
+++ include/world.hpp
@@ -7,10 +7,12 @@ class Game;
 
 class World{
     RLEChunk chunks [16];
+    Game* game;
     int dim;
 
     public:
     void init(int dim, Game* game);
+    void gen_chunk(glm::vec3 pos, int id);
 };
 
 #endif
--- src/world.cpp
+++ src/world.cpp
@@ -1,21 +1,38 @@
+#include <random>
+
 #include "component.hpp"
 #include "world.hpp"
 
 void
 World::init(int dim, Game* game){
     this->dim = dim;
-	int array[] = {0, 1, 1, 1, 1,
-	               1, 1, 0, 0, 0,
-	               0, 0, 1, 1, 1,
-	               0, 0, 1, 1, 1,
-	               0, 0, 1, 1, 1,
-	               1, 1 };
+    this->game = game;
 
     for(int i = 0; i < dim*dim; i++){
-        chunks[i].init(array,dim);
-        std::vector<glm::vec3> verts = chunks[i].less_naive();
         glm::vec3 pos ((i%dim)*dim, 0, (i/dim)*dim);
-        int id = game->new_component(pos);
-	    game->vaos.add_vao((GLfloat*)verts.data(), 3*verts.size(), id);
+	    gen_chunk(pos,i);
     }
+}
+
+void
+World::gen_chunk(glm::vec3 pos, int id){
+    int chunk[dim][dim][dim];
+    std::mt19937 mt(id);
+    std::uniform_int_distribution<int> dist(1,dim);
+
+    for(int x = 0; x < dim; x++){
+        for(int z = 0; z < dim; z++){
+            // int height = 4;
+            int height = dist(mt);
+            for(int y = 0; y < dim; y++){
+                if(y < height) chunk[x][y][z] = 1;
+                else chunk[x][y][z] = 0;
+            }
+        }
+    }
+
+    chunks[id].init((int*)chunk, dim);
+    std::vector<glm::vec3> verts = chunks[id].less_naive();
+    int comp_id = game->new_component(pos);
+	game->vaos.add_vao((GLfloat*)verts.data(), 3*verts.size(), comp_id);
 }
